45a80e42fe31e631a089ae472833d3220126831f
Marco Gerstmann <marco@devtypes.com>
2020-01-05 00:28:04 +0100

    fix dozer mapping (@Data annotation necessary)

--- src/main/java/com/devtypes/retrospec/journal/Journal.java
+++ src/main/java/com/devtypes/retrospec/journal/Journal.java
@@ -1,11 +1,15 @@
 package com.devtypes.retrospec.journal;
 
 import com.devtypes.retrospec.common.base.BaseEntityModel;
+import lombok.Data;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
+@Data
 @Entity
+@ParametersAreNonnullByDefault
 public class Journal extends BaseEntityModel {
 
     @NotBlank
--- src/main/java/com/devtypes/retrospec/journal/JournalVo.java
+++ src/main/java/com/devtypes/retrospec/journal/JournalVo.java
@@ -1,16 +1,16 @@
 package com.devtypes.retrospec.journal;
 
 import com.devtypes.retrospec.common.base.BaseVo;
-import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import javax.validation.constraints.NotBlank;
 
+@Data
 @ParametersAreNonnullByDefault
 @NoArgsConstructor
-@AllArgsConstructor
 @Setter
 public class JournalVo extends BaseVo {
 
