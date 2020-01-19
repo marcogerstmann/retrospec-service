35eb181152ccb762a2ea5cc0ccd3574b5e5ded0d
Marco Gerstmann <marco@devtypes.com>
2020-01-19 18:40:10 +0100

    [#170783818] Add a test

--- src/test/groovy/com/devtypes/retrospec/habit/HabitsControllerSpec.groovy
+++ src/test/groovy/com/devtypes/retrospec/habit/HabitsControllerSpec.groovy
@@ -1,5 +1,7 @@
 package com.devtypes.retrospec.habit
 
+import com.devtypes.retrospec.common.enums.RetrospecEntity
+import com.devtypes.retrospec.common.exception.RetrospecNotFoundException
 import spock.lang.Specification
 
 class HabitsControllerSpec extends Specification {
@@ -10,14 +12,29 @@ class HabitsControllerSpec extends Specification {
         controller = new HabitsController(habitService)
     }
 
-    def "index endpoint should return a list of all habits"() {
+    def "GET endpoint should return a list of all habits"() {
         given: "a list of habits"
         habitService.findAll() >> [new HabitVo(), new HabitVo()]
 
-        when: "index endpoint is called"
+        when: "GET endpoint is called"
         def endpointResult = controller.index()
 
         then: "the list of habits is returned"
         endpointResult.body.size() == 2
     }
+
+    def "GET /{id} should throw 404 if habit is not found"() {
+        given: "an empty return value from the service"
+        habitService.findById(_ as UUID) >> Optional.empty()
+
+        when: "GET /{id} endpoint is called"
+        controller.get(UUID.randomUUID())
+
+        then: "not found exception is thrown"
+        def thrown = thrown(RetrospecNotFoundException)
+
+        and: "entity type of exception is HABIT"
+        thrown.notFoundEntity == RetrospecEntity.HABIT
+    }
+
 }
